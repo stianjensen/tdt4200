649c357fc0c43e714e5cc8e1072cdf1a018afce3
Stian Jensen <me@stianj.com>
2014-10-20 21:13:59 +0200

    reindent

--- ps5/code/raycast.cu
+++ ps5/code/raycast.cu
@@ -23,7 +23,7 @@ stack_t* new_stack(){
     stack->size = 0;
     stack->buffer_size = 1024;
     stack->pixels = (int3*)malloc(sizeof(int3)*1024);
-    
+
     return stack;
 }
 
@@ -34,7 +34,7 @@ void push(stack_t* stack, int3 p){
         stack->pixels = (int3*)malloc(sizeof(int3)*stack->buffer_size);
         memcpy(stack->pixels, temp, sizeof(int3)*stack->buffer_size/2);
         free(temp);
-        
+
     }
     stack->pixels[stack->size] = p;
     stack->size += 1;
@@ -51,7 +51,7 @@ float3 cross(float3 a, float3 b){
     c.x = a.y*b.z - a.z*b.y;
     c.y = a.z*b.x - a.x*b.z;
     c.z = a.x*b.y - a.y*b.x;
-    
+
     return c;
 }
 
@@ -60,7 +60,7 @@ float3 normalize(float3 v){
     v.x /= l;
     v.y /= l;
     v.z /= l;
-    
+
     return v;
 }
 
@@ -68,7 +68,7 @@ float3 add(float3 a, float3 b){
     a.x += b.x;
     a.y += b.y;
     a.z += b.z;
-    
+
     return a;
 }
 
@@ -76,7 +76,7 @@ float3 scale(float3 a, float b){
     a.x *= b;
     a.y *= b;
     a.z *= b;
-    
+
     return a;
 }
 
@@ -99,25 +99,25 @@ void print_properties(){
 // Fills data with values
 unsigned char func(int x, int y, int z){
     unsigned char value = rand() % 20;
-    
+
     int x1 = 300;
     int y1 = 400;
     int z1 = 100;
     float dist = sqrt((x-x1)*(x-x1) + (y-y1)*(y-y1) + (z-z1)*(z-z1));
-    
+
     if(dist < 100){
         value  = 30;
     }
-    
+
     x1 = 100;
     y1 = 200;
     z1 = 400;
     dist = sqrt((x-x1)*(x-x1) + (y-y1)*(y-y1) + (z-z1)*(z-z1));
-    
+
     if(dist < 50){
         value = 50;
     }
-    
+
     if(x > 200 && x < 300 && y > 300 && y < 500 && z > 200 && z < 300){
         value = 45;
     }
@@ -129,7 +129,7 @@ unsigned char func(int x, int y, int z){
 
 unsigned char* create_data(){
     unsigned char* data = (unsigned char*)malloc(sizeof(unsigned char) * DATA_DIM*DATA_DIM*DATA_DIM);
-    
+
     for(int i = 0; i < DATA_DIM; i++){
         for(int j = 0; j < DATA_DIM; j++){
             for(int k = 0; k < DATA_DIM; k++){
@@ -137,7 +137,7 @@ unsigned char* create_data(){
             }
         }
     }
-    
+
     return data;
 }
 
@@ -146,7 +146,7 @@ int inside(float3 pos){
     int x = (pos.x >= 0 && pos.x < DATA_DIM-1);
     int y = (pos.y >= 0 && pos.y < DATA_DIM-1);
     int z = (pos.z >= 0 && pos.z < DATA_DIM-1);
-    
+
     return x && y && z;
 }
 
@@ -154,10 +154,10 @@ int inside(int3 pos){
     int x = (pos.x >= 0 && pos.x < DATA_DIM);
     int y = (pos.y >= 0 && pos.y < DATA_DIM);
     int z = (pos.z >= 0 && pos.z < DATA_DIM);
-    
+
     return x && y && z;
 }
-        
+
 // Indexing function (note the argument order)
 int index(int z, int y, int x){
     return z * DATA_DIM*DATA_DIM + y*DATA_DIM + x;
@@ -168,30 +168,30 @@ float value_at(float3 pos, unsigned char* data){
     if(!inside(pos)){
         return 0;
     }
-    
+
     int x = floor(pos.x);
     int y = floor(pos.y);
     int z = floor(pos.z);
-    
+
     int x_u = ceil(pos.x);
     int y_u = ceil(pos.y);
     int z_u = ceil(pos.z);
-    
+
     float rx = pos.x - x;
     float ry = pos.y - y;
     float rz = pos.z - z;
-    
+
     float a0 = rx*data[index(z,y,x)] + (1-rx)*data[index(z,y,x_u)];
     float a1 = rx*data[index(z,y_u,x)] + (1-rx)*data[index(z,y_u,x_u)];
     float a2 = rx*data[index(z_u,y,x)] + (1-rx)*data[index(z_u,y,x_u)];
     float a3 = rx*data[index(z_u,y_u,x)] + (1-rx)*data[index(z_u,y_u,x_u)];
-    
+
     float b0 = ry*a0 + (1-ry)*a1;
     float b1 = ry*a2 + (1-ry)*a3;
-    
+
     float c0 = rz*b0 + (1-rz)*b1;
 
-    
+
     return c0;
 }
 
@@ -199,37 +199,37 @@ float value_at(float3 pos, unsigned char* data){
 // Serial ray casting
 unsigned char* raycast_serial(unsigned char* data, unsigned char* region){
     unsigned char* image = (unsigned char*)malloc(sizeof(unsigned char)*IMAGE_DIM*IMAGE_DIM);
-    
+
     // Camera/eye position, and direction of viewing. These can be changed to look
     // at the volume from different angles.
     float3 camera = {.x=1000,.y=1000,.z=1000};
     float3 forward = {.x=-1, .y=-1, .z=-1};
     float3 z_axis = {.x=0, .y=0, .z = 1};
-    
+
     // Finding vectors aligned with the axis of the image
     float3 right = cross(forward, z_axis);
     float3 up = cross(right, forward);
-    
+
     // Creating unity lenght vectors
     forward = normalize(forward);
     right = normalize(right);
     up = normalize(up);
-    
+
     float fov = 3.14/4;
     float pixel_width = tan(fov/2.0)/(IMAGE_DIM/2);
     float step_size = 0.5;
-    
+
     // For each pixel
     for(int y = -(IMAGE_DIM/2); y < (IMAGE_DIM/2); y++){
         for(int x = -(IMAGE_DIM/2); x < (IMAGE_DIM/2); x++){
-            
+
             // Find the ray for this pixel
             float3 screen_center = add(camera, forward);
             float3 ray = add(add(screen_center, scale(right, x*pixel_width)), scale(up, y*pixel_width));
             ray = add(ray, scale(camera, -1));
             ray = normalize(ray);
             float3 pos = camera;
-            
+
             // Move along the ray, we stop if the color becomes completely white,
             // or we've done 5000 iterations (5000 is a bit arbitrary, it needs 
             // to be big enough to let rays pass through the entire volume)
@@ -241,12 +241,12 @@ unsigned char* raycast_serial(unsigned char* data, unsigned char* region){
                 int r = value_at(pos, region);                  // Check if we're in the region
                 color += value_at(pos, data)*(0.01 + r) ;       // Update the color based on data value, and if we're in the region
             }
-            
+
             // Write final color to image
             image[(y+(IMAGE_DIM/2)) * IMAGE_DIM + (x+(IMAGE_DIM/2))] = color > 255 ? 255 : color;
         }
     }
-    
+
     return image;
 }
 
@@ -255,7 +255,7 @@ unsigned char* raycast_serial(unsigned char* data, unsigned char* region){
 int similar(unsigned char* data, int3 a, int3 b){
     unsigned char va = data[a.z * DATA_DIM*DATA_DIM + a.y*DATA_DIM + a.x];
     unsigned char vb = data[b.z * DATA_DIM*DATA_DIM + b.y*DATA_DIM + b.x];
-    
+
     int i = abs(va-vb) < 1;
     return i;
 }
@@ -264,17 +264,17 @@ int similar(unsigned char* data, int3 a, int3 b){
 // Serial region growing, same algorithm as in assignment 2
 unsigned char* grow_region_serial(unsigned char* data){
     unsigned char* region = (unsigned char*)calloc(sizeof(unsigned char), DATA_DIM*DATA_DIM*DATA_DIM);
-    
+
     stack_t* stack = new_stack();
-    
+
     int3 seed = {.x=50, .y=300, .z=300};
     push(stack, seed);
     region[seed.z *DATA_DIM*DATA_DIM + seed.y*DATA_DIM + seed.x] = 1;
-    
+
     int dx[6] = {-1,1,0,0,0,0};
     int dy[6] = {0,0,-1,1,0,0};
     int dz[6] = {0,0,0,0,-1,1};
-    
+
     while(stack->size > 0){
         int3 pixel = pop(stack);
         for(int n = 0; n < 6; n++){
@@ -282,33 +282,33 @@ unsigned char* grow_region_serial(unsigned char* data){
             candidate.x += dx[n];
             candidate.y += dy[n];
             candidate.z += dz[n];
-            
+
             if(!inside(candidate)){
                 continue;
             }
-            
+
             if(region[candidate.z * DATA_DIM*DATA_DIM + candidate.y*DATA_DIM + candidate.x]){
                 continue;
             }
-            
+
             if(similar(data, pixel, candidate)){
                 push(stack, candidate);
                 region[candidate.z * DATA_DIM*DATA_DIM + candidate.y*DATA_DIM + candidate.x] = 1;
             }
         }
     }
-    
+
     return region;
 }
 
 
 __global__ void raycast_kernel(unsigned char* data, unsigned char* image, unsigned char* region){
-    
+
 }
 
 
 __global__ void raycast_kernel_texture(unsigned char* image){
-    
+
 }
 
 
@@ -340,17 +340,16 @@ unsigned char* grow_region_gpu(unsigned char* data){
 unsigned char* grow_region_gpu_shared(unsigned char* data){
     return NULL;
 }
-    
 
 int main(int argc, char** argv){
-    
+
     print_properties();
-    
+
     unsigned char* data = create_data();
-    
+
     unsigned char* region = grow_region_serial(data);
-    
+
     unsigned char* image = raycast_serial(data, region);
-    
+
     write_bmp(image, IMAGE_DIM, IMAGE_DIM);
 }
